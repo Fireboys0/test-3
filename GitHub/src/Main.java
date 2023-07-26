285a14bea05760bb9b965c5f982b0b46ee564a98
Fireboys0 <constantinnoel6120@gmail.com>
2023-07-26 10:17:31 +0200

    création de la fonction first (CN)

--- GitHub/src/Main.java
+++ GitHub/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("WOW");
+        first();
+    }
+    static void first () {
+        Scanner abc = new Scanner(System.in);
+        System.out.println("Quel est votre nombre préférer ?");
+        int a = abc.nextInt();
+        System.out.println("Votre nombre préférer est le nombre " + a);
     }
 }
