d57d6fca9c99251e7df8077bbf25e2e418163309
Fireboys0 <constantinnoel6120@gmail.com>
2023-07-26 10:12:15 +0200

    création de la fonction first

--- GitHub/src/Main.java
+++ GitHub/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("WOW");
+        first();
+    }
+     static void first() {
+        Scanner abc = new Scanner(System.in);
+        System.out.println("Votre couleur préférer ?");
+        String a = abc.nextLine();
+        System.out.println("Votre couleur préférer est le "+a);
+
     }
 }
